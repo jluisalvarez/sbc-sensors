dd5373edee0f5c7567f8ae0f8a46207ff77b31c2
J.L. Alvarez <alvarez@uhu.es>
2021-02-25 20:35:59 +0100

    Commit Inicial

--- server.py
+++ server.py
@@ -7,6 +7,63 @@ import os
 import json
 import time
 import psutil
+
+# Gyroscope Sensor HC-SR04
+# Library smbus
+# PIN: RBPi --> TRIG=25, ECHO=24     BBB -->
+import smbus
+import math
+
+def readGyroscope():
+    power_mgmt_1 = 0x6b
+    power_mgmt_2 = 0x6c
+
+    bus = smbus.SMBus(1) # bus = smbus.SMBus(0) fuer Revision 1
+    address = 0x68       # via i2cdetect
+ 
+    # Aktivieren, um das Modul ansprechen zu koennen
+    us.write_byte_data(address, power_mgmt_1, 0)
+    
+    x = read_word_2c(0x43)
+    y = read_word_2c(0x45)
+    z = read_word_2c(0x47)
+
+    gyroscope_data = { 'x': x, 'y':y, 'z':z}
+
+    return gyroscope_data
+
+
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+ 
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+ 
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+ 
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+ 
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+ 
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+ 
+
+
+
+
 import RPi.GPIO as GPIO
 
 # Ultrasonic Sensor HC-SR04
@@ -132,10 +189,24 @@ def read_data():
 
     DistSensor = {"value": dist, "cpu": pcpuDist, "time": tpoDist}
 
+    t1 = time.perf_counter()
+    gyr = readGyroscope()
+    t2 = time.perf_counter()
+    cpu_percents = []
+    for iter in range(1):
+        cpu_percents.append(monitor_cpu(target=readGyroscope))
+    Sum = sum(cpu_percents)
+    total = len(cpu_percents)
+    pcpuGyr = round((Sum / total), 2)
+    pcpuGyr = round((t2 - t1), 2)
+
+    GyrSensor = {"value": gyr, "cpu": pcpuGyr, "time": pcpuGyr}
+
     data = {
         "temperatureSensor": TempSensor,
         "humiditySensor": HumSensor,
         "ultrasonicSensor": DistSensor,
+        "gyroscopeSensor": GyrSensor
     }
     return data
 
